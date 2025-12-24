66a853aa8d62c9e2d698ac633d322b02b722def4
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-12-24 13:23:15 +0530

    fix: add boolean labels for filter display in print/pdf
    (cherry picked from commit 27879ab4a065684c41d80cf598de241d36142da7)

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -44,6 +44,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			this.setup_events,
 		].map((fn) => fn.bind(this));
 		this.init_promise = frappe.run_serially(tasks);
+		this.boolean_labels = { 1: __("Yes"), 0: __("No") };
 		return this.init_promise;
 	}
 
@@ -1524,6 +1525,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 
 	get_filters_html_for_print() {
 		const applied_filters = this.get_filter_values();
+
 		return Object.keys(applied_filters)
 			.map((fieldname) => {
 				const docfield = frappe.query_report.get_filter(fieldname).df;
@@ -1533,8 +1535,16 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 					return null;
 				}
 
+				let display_value = value;
+
+				if (docfield.fieldtype === "Check") {
+					display_value = this.boolean_labels[value];
+				} else {
+					display_value = frappe.format(value, docfield);
+				}
+
 				return `<div class="filter-row">
-					<b>${__(docfield.label, null, docfield.parent)}:</b> ${frappe.format(value, docfield)}
+					<strong>${__(docfield.label, null, docfield.parent)}:</strong> ${display_value}
 				</div>`;
 			})
 			.join("");
@@ -1560,8 +1570,6 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			});
 		}
 
-<<<<<<< HEAD
-=======
 		if (this.report_settings.export_hidden_cols) {
 			const hidden_fields = new Set();
 
@@ -1588,7 +1596,6 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 				);
 			}
 		}
->>>>>>> 68144462eb (refactor: Remove hidden column duplication in desc)
 		this.export_dialog = frappe.report_utils.get_export_dialog(
 			__(this.report_name),
 			extra_fields,
@@ -1645,14 +1652,13 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 	}
 
 	get_applied_filters(filters) {
-		const boolean_labels = { 1: __("Yes"), 0: __("No") };
 		const applied_filters = {};
 
 		for (const [key, value] of Object.entries(filters)) {
 			const df = frappe.query_report.get_filter(key).df;
 			if (!df.hidden_due_to_dependency) {
 				applied_filters[df.label] =
-					df.fieldtype === "Check" ? boolean_labels[value] : value;
+					df.fieldtype === "Check" ? this.boolean_labels[value] : value;
 			}
 		}
 
