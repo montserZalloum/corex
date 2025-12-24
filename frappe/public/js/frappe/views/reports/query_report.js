c886b5478b3105ac696bb00fc998e547c844f70c
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-12-24 12:39:58 +0530

    refactor: Remove hidden column duplication in desc
    (cherry picked from commit 68144462eb5c99fa0ed86f8c80dc96a230c61954)
    
    # Conflicts:
    #	frappe/public/js/frappe/views/reports/query_report.js

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1560,6 +1560,35 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			});
 		}
 
+<<<<<<< HEAD
+=======
+		if (this.report_settings.export_hidden_cols) {
+			const hidden_fields = new Set();
+
+			this.columns.forEach((column) => {
+				if (column.hidden) {
+					hidden_fields.add(column.label);
+				}
+			});
+
+			if (hidden_fields.size) {
+				extra_fields.push(
+					{
+						fieldname: "column_break_1",
+						fieldtype: "Column Break",
+					},
+					{
+						label: __("Include hidden columns"),
+						fieldname: "include_hidden_columns",
+						fieldtype: "Check",
+						description: __("Hidden columns include: <br> {0}", [
+							frappe.utils.comma_and(Array.from(hidden_fields)),
+						]),
+					}
+				);
+			}
+		}
+>>>>>>> 68144462eb (refactor: Remove hidden column duplication in desc)
 		this.export_dialog = frappe.report_utils.get_export_dialog(
 			__(this.report_name),
 			extra_fields,
