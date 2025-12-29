e7a3f7abe27672696d19fe918c3a67bd7ce076a0
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-12-29 16:40:49 +0530

    fix: correct boolean label handling for filter display
    (cherry picked from commit 0001c47a2c52552ca37925066f1249365d27bd15)

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -1538,7 +1538,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 				let display_value = value;
 
 				if (docfield.fieldtype === "Check") {
-					display_value = this.boolean_labels[value];
+					display_value = this.boolean_labels[cint(value)];
 				} else {
 					display_value = frappe.format(value, docfield);
 				}
@@ -1658,7 +1658,7 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 			const df = frappe.query_report.get_filter(key).df;
 			if (!df.hidden_due_to_dependency) {
 				applied_filters[df.label] =
-					df.fieldtype === "Check" ? this.boolean_labels[value] : value;
+					df.fieldtype === "Check" ? this.boolean_labels[cint(value)] : value;
 			}
 		}
 
