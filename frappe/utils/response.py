8d00595f63620e40eb4918c1fc859ca0018abee6
ljain112 <ljain112@gmail.com>
2026-01-21 19:10:12 +0530

    fix: update Content-Disposition header for binary as attachment
    (cherry picked from commit 4a9af04f94a173d1df9a1ff7f9beb31c48aa4e99)

--- frappe/utils/response.py
+++ frappe/utils/response.py
@@ -156,7 +156,7 @@ def as_binary():
 	response.mimetype = "application/octet-stream"
 	filename = frappe.response["filename"]
 	filename = filename.encode("utf-8").decode("unicode-escape", "ignore")
-	response.headers.add("Content-Disposition", None, filename=filename)
+	response.headers.add("Content-Disposition", "attachment", filename=filename)
 	response.data = frappe.response["filecontent"]
 	return response
 
